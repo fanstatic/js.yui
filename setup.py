6d4c81d6da11f15ac94a7041c8b6e0fed7dcd742
Jan-Jaap Driessen <devnull@localhost>
2011-01-07 12:14:38 +0100

    Back to development: 2.8.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '2.8.2'
+version = '2.8.3dev'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
